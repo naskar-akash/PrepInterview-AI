caaf200f9e1c225a559f6c863747bc7eaadee734
naskar-akash <naskar.akash1947@gmail.com>
2026-05-25 11:37:29 +0530

    installing react

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className= "bg-gray-800 text-white h-screen flex items-center justify-center">
+      app
+    </div>
+  )
+}
+
+export default App
