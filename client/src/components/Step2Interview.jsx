fe886c17c55628171b0d643f614ea4e3473da668
naskar-akash <naskar.akash1947@gmail.com>
2026-07-09 22:28:02 +0530

    completing speak and video synchronisation

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -6,7 +6,7 @@ import { motion } from "motion/react";
 import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
 
 const Step2Interview = ({ interviewData, onFinish }) => {
-  const { interview_id, questions, userName } = interviewData;
+  const { interview_id, questions, username } = interviewData;
   const [isMicOn, setIsMicOn] = useState(false);
   const [isIntroPhase, setIsIntroPhase] = useState(true);
   const [isAiPlaying, setIsAiPlaying] = useState(false);
@@ -97,15 +97,67 @@ const Step2Interview = ({ interviewData, onFinish }) => {
         videoRef.current?.pause()
         videoRef.current.currentTime = 0;
         setIsAiPlaying(false);
-      }
 
+        // Setting subtitles
+        setTimeout(() => {
+          setSubtitle("");
+          resolve();
+        }, 300)
+      };
+
+      setSubtitle(text);
+      window.speechSynthesis.speak(utterance);
     })
   }
 
+  // To call speakText function
+  useEffect(() => {
+    if (! selectedVoice) {
+      return;
+    }
+    const runIntro = async () => {
+      if (isIntroPhase) {
+        await speakText(`Hi ${username.split(" ")[0] || username }, it's great to meet you today. I hope you're feeling confident and ready.`);
+        await speakText(`I'll ask you a few questions. Just answer naturally, and take your time. Let's begin.`);
+        setIsIntroPhase(false)
+      }else if(currentQuestion) {
+        await new Promise(r => setTimeout(r, 800));
+        // If last question (hard level)
+        if(currentIndex === questions.length - 1){
+          await speakText(`Alright, this one might be a bit challenging. Take your time and give it your best.`);
+        }
+        await speakText(currentQuestion.question);
+      }
+    }
+    runIntro()
+    
+  }, [selectedVoice, isIntroPhase, currentIndex])
+
+  // Timer setup
+  useEffect(() => {
+    if(isIntroPhase) return;
+    if(!currentQuestion) return;
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => {
+        if(prev <= 1){
+          clearInterval(timer);
+          return 0;
+        }
+        return prev - 1;
+      })
+    }, 1000);
+    return ()=> clearInterval(timer);
+
+  }, [isIntroPhase, currentIndex])
+  
+  
+
   return (
     <div className="min-h-screen bg-linear-to-br from-lime-200 via-gray-50 to-teal-200 flex items-center justify-center p-4 sm:p-6">
+
       {/* data mount -> load voice -> intro speak -> question speak -> mic on -> timer running -> submit -> feedback speed -> next question -> repeat -> finish */}
       <div className="w-full max-w-350 min-h-[80vh] flex rounded-3xl flex-col lg:flex-row border border-gray-200 overflow-hidden shadow-2xl bg-white">
+
         {/* video section */}
         <div className="w-full flex flex-col items-center lg:w-[35%] bg-white p-6 space-y-6 border-r border-gray-200">
           <div className="w-full max-w-md rounded-2xl overflow-hidden shadow-xl">
@@ -119,19 +171,25 @@ const Step2Interview = ({ interviewData, onFinish }) => {
               className="w-full h-auto object-cover"
             />
           </div>
+
           {/* subtitle area */}
+          {subtitle && (
+            <div className="w-full max-w-md bg-gray-50 border border-gray-200 rounded-xl p-4 shadow-sm">
+              <p className="text-gray-700 text-sm sm:text-base font-medium text-center leading-relaxed">{subtitle}</p>
+            </div>
+          )}
 
           {/* timer area */}
           <div className="w-full max-w-md bg-white p-6 rounded-2xl border border-gray-200 shadow-md space-y-5">
             <div className="flex justify-between items-center">
               <span className="text-sm text-gray-500">Interview Status</span>
-              <span className="text-sm font-semibold text-emerald-500">
-                AI Speaking
-              </span>
+              {isAiPlaying && <span className="text-sm font-semibold text-emerald-500">
+                {isAiPlaying ? "AI Speaking" : ""}
+              </span>}
             </div>
             <div className="h-px bg-gray-200"></div>
             <div className="flex justify-center">
-              <Timer timeLeft={24} totalTime={60} />
+              <Timer timeLeft={timeLeft} totalTime={currentQuestion?.timelimit} />
             </div>
             <div className="h-px bg-gray-200"></div>
             <div className="grid grid-cols-2 gap-6 text-center">
@@ -155,17 +213,19 @@ const Step2Interview = ({ interviewData, onFinish }) => {
           <h2 className="text-xl sm:text-2xl font-bold text-emerald-500 mb-6">
             AI Smart Interview
           </h2>
-          <div className="relative mb-6 bg-stone-50 p-4 sm:p-6 rounded-2xl border border-gray-200 shadow-sm">
+          {! isIntroPhase && <div className="relative mb-6 bg-stone-50 p-4 sm:p-6 rounded-2xl border border-gray-200 shadow-sm">
             <p className="text-xs sm:text-sm text-gray-500 mb-2">
               Question {currentIndex + 1} of {questions.length}
             </p>
             <div className="text-base sm:text-lg font-semibold text-gray-800 leading-relaxed">
               {currentQuestion?.question}
             </div>
-          </div>
+          </div>}
           <textarea
             name=""
             id=""
+            value={answer}
+            onChange={(e)=>setAnswer(e.target.value)}
             placeholder="Type your answer here..."
             className="flex-1 bg-neutral-100 p-4 sm:p-6 rounded-2xl resize-none outline-none border border-gray-200 focus:ring-2 focus:ring-emerald-400 transition text-gray-800"
           />
