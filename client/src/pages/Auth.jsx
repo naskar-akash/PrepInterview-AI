f2c8fea883fc997f4550fa594474aec47a541fa3
naskar-akash <naskar.akash1947@gmail.com>
2026-08-04 08:32:32 +0530

    debugging

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -19,7 +19,6 @@ const Auth = ({isModel = false}) => {
     try {
     const googleResponse = await signInWithPopup(auth, provider);
 
-    console.log(googleResponse) // to be removed in production, just for debugging purposes
 
     const data = {
       name: googleResponse.user.displayName,
